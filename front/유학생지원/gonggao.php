af4409a68b5726eab7e9e875c6e83794fc021e5f
zhishiqiu <leiyating407@gmail.com>
2023-05-18 20:33:35 +0900

    ...

---
+++ front/유학생지원/gonggao.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Bulletin Board</title>
+  <link rel="stylesheet" href="css/gonggao.css" />
+</head>
+
+<body>
+  <header class="header">
+    <div class="container">
+      <ul class="nav">
+        <li class="active" id="firstLink"><a href="index.php">Home</a></li>
+      </ul>
+    </div>
+  </header>
+
+
+
+
+
+
+
+</body>
+
+
+</html>

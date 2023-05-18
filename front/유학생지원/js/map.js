338237f1ec8a8006d0a9d50bfd25804f5ee8e47e
halikes <54992344+halikes@users.noreply.github.com>
2023-05-18 18:58:39 +0900

    update
    更新了地图功能

---
+++ front/유학생지원/js/map.js
@@ -0,0 +1,85 @@
+let map;
+let maxZoomService;
+let infoWindow;
+
+function initMap() {
+    
+    // 创建地图实例，设置初始中心位置和缩放级别
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 37.2964, lng: 126.8351},
+        zoom: 16
+    });
+    infoWindow = new google.maps.InfoWindow();
+    maxZoomService = new google.maps.MaxZoomService();
+
+    // 在地图上添加学校标记
+    var schoolMarker = new google.maps.Marker({
+        position: {lat: 37.2964, lng: 126.8351},
+        map: map,
+        title: 'Hangyang University Erica'
+    });
+
+    // 添加更多的地图标记
+    var marker1 = new google.maps.Marker({
+        position: {lat: 37.2964, lng: -122.42},
+        map: map,
+        title: '标记1'
+    });
+
+    var marker2 = new google.maps.Marker({
+        position: {lat: 37.2964, lng: 126.8351},
+        map: map,
+        title: '标记2'
+    });
+
+    // // 自定义标记图标
+    // var marker = new google.maps.Marker({
+    //     position: {lat: 37.2964, lng: 126.8351},
+    //     map: map,
+    //     title: '标记',
+    //     icon: 'marker.png' // 指定图标图片路径
+    // });
+
+    // 添加信息窗口
+    var infowindow = new google.maps.InfoWindow({
+        content: '这是一个标记的信息'
+    });
+
+    // 点击标记时显示信息窗口
+    marker.addListener('click', function() {
+    infowindow.open(map, marker);
+    });
+
+    marker1.addListener('click', function() {
+        // 点击标记1时的操作
+        // 可以显示详细信息、导航到该地点等
+    });
+
+    marker2.addListener('click', function() {
+        // 点击标记2时的操作
+        // 可以显示详细信息、导航到该地点等
+    });
+
+    // 添加缩放控件
+    var zoomControl = new google.maps.ZoomControl();
+    map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(zoomControl);
+
+    // 添加导航控件
+    var directionsService = new google.maps.DirectionsService();
+    var directionsRenderer = new google.maps.DirectionsRenderer();
+    directionsRenderer.setMap(map);
+    var origin = new google.maps.LatLng(37.7749, -122.4194);
+    var destination = new google.maps.LatLng(37.775, -122.42);
+    var request = {
+        origin: origin,
+        destination: destination,
+        travelMode: google.maps.TravelMode.WALKING
+    };
+    directionsService.route(request, function(result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsRenderer.setDirections(result);
+        }
+    });
+
+}
+
--- front/유학생지원/map.php
+++ front/유학생지원/map.php
@@ -1,20 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HYU ERICA</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh; /* 使父元素铺满整个视口高度 */
+        }
+        #map {
 
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha384-tuoy5Pw70jwKkqs/5Np+jn5ny9uew3ZnSlgIt9q+3VltgIhUa9QWTqZZ4wO9DYDr" crossorigin=""/>
+            width: 1000px;
+            height: 800px;
+        }
+    </style>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB0aQl1Flcfgq695fxCi_VW63P2ORvM0FI&callback=initMap" async defer></script>
+    <script src="map.js"></script>
+</head>
+<body>
+    <div id="map"></div>
+    <script src="js/map.js"></script>
+</body>
+</html>
 
-<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha384-S6W24xD+Xr5z5rK1hO56mKyyUMFJ/cisRH+W1r0iQgG3TlzqJJz9eLzFXCCM8jlF" crossorigin=""></script>
-
-<div id="map">
-// 创建地图对象
-var myMap = L.map('map').setView([51.505, -0.09], 13);
-
-// 添加地图瓦片图层
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-    maxZoom: 18,
-}).addTo(myMap);
-
-// 添加标记
-var marker = L.marker([51.5, -0.09]).addTo(myMap);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-
-</div>

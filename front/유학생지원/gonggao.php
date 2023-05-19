a809529a3ae6e77d684cb9d2c3ec692d4a6468bf
zhishiqiu <leiyating407@gmail.com>
2023-05-20 00:29:22 +0900

    。。。

--- front/유학생지원/gonggao.php
+++ front/유학생지원/gonggao.php
@@ -6,17 +6,53 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Bulletin Board</title>
   <link rel="stylesheet" href="css/gonggao.css" />
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+  <link rel="stylesheet" type="text/css" href="css/isotope.css" media="screen" />
+  <link rel="stylesheet" href="js/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
+  <link href="css/animate.css" rel="stylesheet" media="screen">
+  <link href="flexslider/flexslider.css" rel="stylesheet" />
+  <link href="js/owl-carousel/owl.carousel.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/styles.css" />
+  <!-- Font Awesome -->
+  <link href="font/css/font-awesome.min.css" rel="stylesheet">
 </head>
 
-<body>
+<body style="background-color: #88d1ff;">
+
   <header class="header">
     <div class="container">
-      <ul class="nav">
-        <li class="active" id="firstLink"><a href="index.php">Home</a></li>
-      </ul>
-    </div>
-  </header>
+      <nav class="navbar navbar-inverse" role="navigation">
 
+        <div id="main-nav" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active" id="firstLink"><a href="index.php" class="scroll-link">Home</a></li>
+
+          </ul>
+        </div>
+        <!--/.navbar-collapse-->
+      </nav>
+      <!--/.navbar-->
+    </div>
+    <!--/.container-->
+  </header>
+  <!--header-->
+
+  <div class="wrap">
+    <div class="lnb_wrap">
+      <div class="lnb">
+        <div class="theme">
+          <h1>공지</h1>
+        </div>
+        <ul>
+          <li class="one"><a href="#">학사공지 ></a></li>
+          <li class="two"><a href="#">입학공지 ></a></li>
+          <li class="three"><a href="#">취업공지 ></a></li>
+          <li class="four"><a href="#">창업공지 ></a></li>
+        </ul>
+      </div>
+
+    </div>
+  </div>
 
 
 
@@ -24,6 +60,16 @@
 
 
 </body>
-
+<script src="js/modernizr-latest.js"></script>
+<script src="js/jquery-1.8.2.min.js" type="text/javascript"></script>
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
+<script src="js/jquery.isotope.min.js" type="text/javascript"></script>
+<script src="js/fancybox/jquery.fancybox.pack.js" type="text/javascript"></script>
+<script src="js/jquery.nav.js" type="text/javascript"></script>
+<script src="js/jquery.fittext.js"></script>
+<script src="js/waypoints.js"></script>
+<script src="flexslider/jquery.flexslider.js"></script>
+<script src="js/custom.js" type="text/javascript"></script>
+<script src="js/owl-carousel/owl.carousel.js"></script>
 
 </html>

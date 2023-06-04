96238b6055380c82c08e262632ad3bc429a0ce29
halikes <54992344+halikes@users.noreply.github.com>
2023-06-04 21:05:17 +0900

    Update userpage.php

--- final-wuhuwuhu/userpage.php
+++ final-wuhuwuhu/userpage.php
@@ -8,29 +8,29 @@
 <html lang="en-gb" class="no-js">
 <!--<![endif]-->
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <!--[if lt IE 9]>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <!--[if lt IE 9]> 
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <![endif]-->
-    <title>Agile Agency Free Bootstrap Web Template</title>
-    <meta name="description" content="">
-    <meta name="author" content="WebThemez">
-    <!--[if lt IE 9]>
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <title>USER PAGE</title>
+  <meta name="description" content="">
+  <meta name="author" content="WebThemez">
+  <!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
-    <!--[if lte IE 8]>
-    <script type="text/javascript" src="http://explorercanvas.googlecode.com/svn/trunk/excanvas.js"></script>
-    <![endif]-->
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/isotope.css" media="screen" />
-    <link rel="stylesheet" href="js/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
-    <link href="css/animate.css" rel="stylesheet" media="screen">
-    <link href="flexslider/flexslider.css" rel="stylesheet" />
-    <link href="js/owl-carousel/owl.carousel.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/styles.css" />
-    <!-- Font Awesome -->
-    <link href="font/css/font-awesome.min.css" rel="stylesheet">
+  <!--[if lte IE 8]>
+		<script type="text/javascript" src="http://explorercanvas.googlecode.com/svn/trunk/excanvas.js"></script>
+	<![endif]-->
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+  <link rel="stylesheet" type="text/css" href="css/isotope.css" media="screen" />
+  <link rel="stylesheet" href="js/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
+  <link href="css/animate.css" rel="stylesheet" media="screen">
+  <link href="flexslider/flexslider.css" rel="stylesheet" />
+  <link href="js/owl-carousel/owl.carousel.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/styles.css" />
+  <!-- Font Awesome -->
+  <link href="font/css/font-awesome.min.css" rel="stylesheet">
 </head>
 
 <body>
